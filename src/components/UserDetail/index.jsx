93709603a6979615ba7c2f5a0e5bd691cbec4f44
Hoang <thhclone2@gmail.com>
2025-10-25 09:19:49 +0700

    new modification

--- src/components/UserDetail/index.jsx
+++ src/components/UserDetail/index.jsx
@@ -1,24 +1,38 @@
 import React from "react";
-import {Typography} from "@mui/material";
-
+import { Typography, Link as MuiLink } from "@mui/material";
+import { useParams, Link } from "react-router-dom";
+import models from "../../modelData/models"; // đường dẫn đến file modelData/models.js
 import "./styles.css";
-import {useParams} from "react-router-dom";
 
-/**
- * Define UserDetail, a React component of Project 4.
- */
 function UserDetail() {
-    const user = useParams();
-    return (
-        <>
-          <Typography variant="body1">
-            This should be the UserDetail view of the PhotoShare app. Since it is
-            invoked from React Router the params from the route will be in property match.
-            So this should show details of user: {user.userId}.
-            You can fetch the model for the user from models.userModel.
-          </Typography>
-        </>
-    );
+  const { userId } = useParams();
+  const user = models.userModel(userId); // Lấy dữ liệu user từ model
+
+  if (!user) {
+    return <Typography variant="body1">User not found.</Typography>;
+  }
+
+  return (
+    <div className="user-detail-container">
+      <Typography variant="h5">
+        {user.first_name} {user.last_name}
+      </Typography>
+      <Typography variant="body1">
+        <strong>Occupation:</strong> {user.occupation}
+      </Typography>
+      <Typography variant="body1">
+        <strong>Location:</strong> {user.location}
+      </Typography>
+      <Typography variant="body1">
+        <strong>Description:</strong> {user.description}
+      </Typography>
+      <Typography variant="body1" sx={{ mt: 2 }}>
+        <MuiLink component={Link} to={`/photos/${userId}`}>
+          View Photos
+        </MuiLink>
+      </Typography>
+    </div>
+  );
 }
 
 export default UserDetail;
--- src/components/UserPhotos/index.jsx
+++ src/components/UserPhotos/index.jsx
@@ -1,23 +1,64 @@
 import React from "react";
-import { Typography } from "@mui/material";
-
+import { Typography, Card, CardContent, CardMedia, Divider, Box } from "@mui/material";
+import { useParams, Link } from "react-router-dom";
+import models from "../../modelData/models";
 import "./styles.css";
-import {useParams} from "react-router-dom";
 
-/**
- * Define UserPhotos, a React component of Project 4.
- */
-function UserPhotos () {
-    const user = useParams();
-    return (
-      <Typography variant="body1">
-        This should be the UserPhotos view of the PhotoShare app. Since it is
-        invoked from React Router the params from the route will be in property
-        match. So this should show details of user:
-        {user.userId}. You can fetch the model for the user
-        from models.photoOfUserModel(userId):
-      </Typography>
-    );
+function UserPhotos() {
+  const { userId } = useParams();
+  const photos = models.photoOfUserModel(userId);
+
+  if (!photos || photos.length === 0) {
+    return <Typography variant="body1">No photos found for this user.</Typography>;
+  }
+
+  return (
+    <div className="user-photos-container">
+      {photos.map((photo) => (
+        <Card key={photo._id} sx={{ mb: 3, p: 2 }}>
+          {/* Ảnh */}
+          <CardMedia
+            component="img"
+            image={`images/${photo.file_name}`}
+            alt="User photo"
+            sx={{
+              width: "100%",
+              maxWidth: 600,
+              borderRadius: 2,
+              boxShadow: 2,
+            }}
+          />
+          <CardContent>
+            {/* Thời gian tạo ảnh */}
+            <Typography variant="body2" color="textSecondary">
+              Uploaded: {new Date(photo.date_time).toLocaleString()}
+            </Typography>
+
+            {/* Comment */}
+            {photo.comments && photo.comments.length > 0 && (
+              <Box sx={{ mt: 2 }}>
+                <Divider sx={{ mb: 1 }} />
+                <Typography variant="subtitle1">Comments:</Typography>
+                {photo.comments.map((comment) => (
+                  <Box key={comment._id} sx={{ mt: 1, mb: 1 }}>
+                    <Typography variant="body2">
+                      <strong>
+                        <Link to={`/users/${comment.user._id}`}>
+                          {comment.user.first_name} {comment.user.last_name}
+                        </Link>
+                      </strong>{" "}
+                      ({new Date(comment.date_time).toLocaleString()}):
+                    </Typography>
+                    <Typography variant="body2">{comment.comment}</Typography>
+                  </Box>
+                ))}
+              </Box>
+            )}
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
 }
 
 export default UserPhotos;
